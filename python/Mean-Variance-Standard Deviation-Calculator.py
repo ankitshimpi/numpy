9798eb4d143bd22d9a7a40abc77aae5c772808d8
Aniruddha Shimpi <164496928+ankitshimpi@users.noreply.github.com>
2024-12-29 19:31:56 +0530

    Update Mean-Variance-Standard Deviation-Calculator.py

--- python/Mean-Variance-Standard Deviation-Calculator.py
+++ python/Mean-Variance-Standard Deviation-Calculator.py
@@ -1,1 +1,17 @@
-
+import numpy as np
+def calculate(data):
+  if len(data) != 9:
+    raise ValueError("List must contain nine numbers.")
+  arr = np.reshape(data, (3, 3))
+  statistics = {
+      'mean': [arr.mean(axis=0).tolist(), arr.mean(axis=1).tolist(), arr.flatten().mean()],
+      'variance': [arr.var(axis=0).tolist(), arr.var(axis=1).tolist(), arr.flatten().var()],
+      'standard deviation': [arr.std(axis=0).tolist(), arr.std(axis=1).tolist(), arr.flatten().std()],
+      'max': [arr.max(axis=0).tolist(), arr.max(axis=1).tolist(), arr.flatten().max()],
+      'min': [arr.min(axis=0).tolist(), arr.min(axis=1).tolist(), arr.flatten().min()],
+      'sum': [arr.sum(axis=0).tolist(), arr.sum(axis=1).tolist(), arr.flatten().sum()]
+  }
+  for key, value in statistics.items():
+    for i in range(len(value)):
+      value[i] = value[i].tolist()
+  return statistics
